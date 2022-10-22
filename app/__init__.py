efdd42da68df74faee332e150fe47f7ee448a49c
Liat <liathirsh@gmail.com>
2022-10-21 20:59:25 -0400

    created a book class and a list of books

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
+# Define the start up logic for the Flask server in this file
+
 from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    from .routes import books_bp
+    app.register_blueprint(books_bp)
 
     return app
---
+++ app/routes.py
@@ -0,0 +1,38 @@
+# The responsibility of this file is to define the endpoints
+# Blueprint is a Flask class that provides a pattern for grouping related routes
+# Flask will often refer to these routes using the word "view"
+
+
+from flask import Blueprint, jsonify
+
+class Book():
+    def __init__ (self, id, name, author, genre, num_in_series=0):
+        self.id = id
+        self.name = name
+        self.author = author
+        self.genre = genre
+        self.num_in_series = num_in_series
+
+BOOKS = [
+    Book(1, "Harry Potter", "JK Rowling", "Fantasy", 7),
+    Book(2, "The Lord of the Rings", "JRR Tolkein", "Fantasy", 3),
+    Book(3, "The Rise and Fall of the Dinosaurs", "Some Dude", "Nonfiction"),
+    Book(4, "I Know Why the Caged Bird Sings", "Maya Angelou", "Memoir")
+
+]
+
+
+books_bp = Blueprint("books", __name__)
+
+@books_bp.route("/books", methods=["GET"])
+def get_all_books():
+    result = []
+    for book in BOOKS:
+        result.append({
+            "id": book.id,
+            "name": book.name,
+            "author": book.author,
+            "genre": book.genre,
+            "number in series": book.num_in_series
+        })
+    return jsonify(result)
